1150691a431f8d4ed9745a28ae59911e15fadb40
noisemaster <psp1001k2006@gmail.com>
2016-06-26 20:05:55 -0400

    changed onReady status change

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -523,7 +523,7 @@ func playSound(play *Play, vc *discordgo.VoiceConnection) (err error) {
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	log.Info("Recieved READY payload")
-	s.UpdateStatus(0, "airhornbot.com")
+	s.UpdateStatus(0, "github.com/noisemaster/airhornbot")
 }
 
 func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {

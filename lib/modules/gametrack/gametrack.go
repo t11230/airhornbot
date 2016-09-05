ab1bef0e85f4018d1f863d6e402a982d6587db68
Tayler <tayler.burns@gmail.com>
2016-09-05 18:50:24 -0400

    Commented code that still needs to be refactored, fixed log messages

--- lib/modules/gametrack/gametrack.go
+++ lib/modules/gametrack/gametrack.go
@@ -1,65 +1,64 @@
 package gametrack
 
-import (
-    "bytes"
-    "fmt"
-    "text/tabwriter"
+// import (
+//     "bytes"
+//     "fmt"
+//     "text/tabwriter"
 
-    "github.com/bwmarrin/discordgo"
-)
+//     "github.com/bwmarrin/discordgo"
+// )
 
-type GameTrackEntry struct {
-    UserID string
-    Game string
-    Time int
-}
+// type GameTrackEntry struct {
+//     UserID string
+//     Game string
+//     Time int
+// }
 
-func gpHandleStatsCommand(guild *discordgo.Guild, message *discordgo.Message, args []string) string {
-    return ""
-}
+// func gpHandleStatsCommand(guild *discordgo.Guild, message *discordgo.Message, args []string) string {
+//     return ""
+// }
 
-func gpPrintStats(guild *discordgo.Guild, user *discordgo.User, args []string) string { 
-    db := dbGetSession(guild.ID)
-    entries := db.GameTrackGetUserStats(user.ID, 10)
+// func gpPrintStats(guild *discordgo.Guild, user *discordgo.User, args []string) string {
+//     db := dbGetSession(guild.ID)
+//     entries := db.GameTrackGetUserStats(user.ID, 10)
 
-    if len(entries) == 0 {
-        return "No stats. Git gud scrub."
-    }
+//     if len(entries) == 0 {
+//         return "No stats. Git gud scrub."
+//     }
 
-    w := &tabwriter.Writer{}
-    buf := &bytes.Buffer{}
+//     w := &tabwriter.Writer{}
+//     buf := &bytes.Buffer{}
 
-    w.Init(buf, 0, 4, 0, ' ', 0)
-    // fmt.Fprintf(w, "%s Game-Time Stats:\n", ) // Not sure how to get nicknames...
-    fmt.Fprintf(w, "```\n")
+//     w.Init(buf, 0, 4, 0, ' ', 0)
+//     // fmt.Fprintf(w, "%s Game-Time Stats:\n", ) // Not sure how to get nicknames...
+//     fmt.Fprintf(w, "```\n")
 
-    for _, entry := range(entries) {
-        fmt.Fprintf(w, "%s: \t %.2f Hours\n", entry.Game, float64(entry.Time) / (60.0*60.0))
-    }
-    
-    fmt.Fprintf(w, "```\n")
-    w.Flush()
-    return buf.String()
-}
+//     for _, entry := range(entries) {
+//         fmt.Fprintf(w, "%s: \t %.2f Hours\n", entry.Game, float64(entry.Time) / (60.0*60.0))
+//     }
 
+//     fmt.Fprintf(w, "```\n")
+//     w.Flush()
+//     return buf.String()
+// }
 
-func processGameplayLoop(ticker *time.Ticker) {
-    for {
-        select {
-        case <- ticker.C:
-            var processedUsers []string
-            for _, g := range discord.State.Guilds {
-                for _, p := range g.Presences {
-                    if p.Game != nil && len(p.Game.Name) > 0 &&
-                            !utils.Scontains(p.User.ID, processedUsers...) {
+// func processGameplayLoop(ticker *time.Ticker) {
+//     for {
+//         select {
+//         case <- ticker.C:
+//             var processedUsers []string
+//             for _, g := range discord.State.Guilds {
+//                 for _, p := range g.Presences {
+//                     if p.Game != nil && len(p.Game.Name) > 0 &&
+//                             !utils.Scontains(p.User.ID, processedUsers...) {
 
-                        processedUsers = append(processedUsers, p.User.ID)
+//                         processedUsers = append(processedUsers, p.User.ID)
 
-                        // db := rdb.GetSession(g.ID)
-                        // db.GameTrackIncGameEntry(p.User.ID, p.Game.Name, 60)
-                    }
-                }
-            }
-        }
-    }
-}
+//                         // db := rdb.GetSession(g.ID)
+//                         // db.GameTrackIncGameEntry(p.User.ID, p.Game.Name, 60)
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
